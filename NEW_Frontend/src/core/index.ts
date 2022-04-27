1285108b3684d132442041f5bf61771f7665224d
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-04-27 15:43:24 +0300

    Templating & Object creation done

--- NEW_Frontend/src/core/index.ts
+++ NEW_Frontend/src/core/index.ts
@@ -1,4 +1,5 @@
 import Router from './router';
 import Component from './component';
-
-export { Router, Component };
+import { createElement } from './createElement';
+import { renderTemplate } from './renderTemplate';
+export { Router, Component, createElement as CreateElement, renderTemplate as RenderTemplate };
--- NEW_Frontend/src/main.js
+++ NEW_Frontend/src/main.js
@@ -1,5 +1,5 @@
-import App from './App';
-import { Router } from './core';
+// import App from './App';
+// import { Router } from './core';
 
 
 if (module.hot) {
@@ -11,12 +11,39 @@ if (module.hot) {
 //   Render(root, App());
 // };
 
-const app = async () => {
-  document.getElementById('app').appendChild(await App());
-};
-window.addEventListener("popstate", Router());
-document.addEventListener("DOMContentLoaded", () => {
-  Router();
-});
+// const app = async () => {
+//   document.getElementById('app').appendChild(await App());
+// };
+// window.addEventListener("popstate", Router());
+// document.addEventListener("DOMContentLoaded", () => {
+//   Router();
+// });
 
-app();
+// app();
+
+import { CreateElement, RenderTemplate } from './core';
+
+const Header = () => (
+ `
+ <h1>I am the header element</h1>
+ `
+)
+
+const html = `
+  <div id="container" style="color: red">
+    Hello world
+    %Header()%
+    %for(const index in this.links ){%
+      <a href="%this.links[index].text%">%this.links[index].text%</a>
+    %}%
+    <p id="text" class="myP">
+      Hello, %name%!
+    </p>
+  </div>
+`
+const template = RenderTemplate(html, { name: 'John', links: [{ text: 'google.com' }, { text: 'facebook.com' }], Header });
+console.log(template);
+console.log(CreateElement(template));
+
+
+//https://dev.to/buttercubz/explained-and-created-a-simple-virtual-dom-from-scratch-5765
