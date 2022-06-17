a2a5e064711cf1047adb578f9af0262705395922
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-06-17 18:24:29 +0300

    Completed domain validation & PUT method

--- Backend/services/domains/services/domain.service.js
+++ Backend/services/domains/services/domain.service.js
@@ -1,6 +1,7 @@
 const { debug } = require('shared').utils.logging;
 const { v4: uuid } = require('uuid');
 const StatusCodeException = require('shared').exceptions.StatusCodeException;
+const { domainSchema } = require('../validation');
 
 exports.getDomainById = async (req) => {
     const domains = await req.db.Domain.findOne({
@@ -17,10 +18,29 @@ exports.getDomainById = async (req) => {
 }
 
 exports.putDomainById = async (req) => {
-    console.log('Ids: ', req.params)
-    console.log('JWToken data is: ', req.locals.token);
-    // TODO: To be implemented
-    return null;
+    await domainSchema.updateDomain.validateAsync(req.body)
+
+    const domain = await req.db.Domain.findOne({
+        _id: req.params.id,
+        userId: req.locals.token.userId
+    })
+
+    if(domain === null) {
+        throw new StatusCodeException('Domain with specified id doesn\'t exists.', 404)
+    }
+
+    const { name, activeUrl, active, users } = req.body
+    domain.name = name ?? domain.name;
+    domain.activeUrl = activeUrl ?? domain.activeUrl; 
+    domain.active = active ?? domain.active;
+    if(domain.users.length > 3) {
+        throw new StatusCodeException('You can add up to 3 users to a domain.', 400)
+    }
+    domain.users = users ?? domain.users;
+
+    await domain.save();
+
+    return '';
 }
 
 exports.deleteDomainById = async (req) => {
@@ -48,6 +68,8 @@ exports.getAllDomains = async (req) => {
 }
 
 exports.createNewDomain = async (req) => {
+    await domainSchema.createDomain.validateAsync(req.body)
+
     const { name, activeUrl } = req.body
     console.log(req.locals.token.userId)
 
