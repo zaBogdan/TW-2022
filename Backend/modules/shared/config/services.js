97885135b61ba2d4d38b1379e001a0532538279d
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-06-18 12:25:12 +0300

    Events linked to gateway & integrated in deployment

--- Backend/modules/shared/config/services.js
+++ Backend/modules/shared/config/services.js
@@ -4,11 +4,14 @@ const development =  {
     GATEWAY: `${basePath}:3000`,
     AUTHENTICATION: `${basePath}:3100`,
     DOMAIN: `${basePath}:3101`,
+    EVENT: `${basePath}:3102`,
 }
 
 const production = {
     GATEWAY: 'http://Gateway:3000',
     AUTHENTICATION: 'http://Authentication:3100',
     DOMAIN: 'http://Domains:3101',
+    EVENT: 'http://Events:3102'
 }
+
 module.exports = (process.env.NODE_ENV === 'prod' ? production : development);
