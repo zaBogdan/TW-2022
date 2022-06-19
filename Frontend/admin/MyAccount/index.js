1011ff5159769363fe5fdc319b52468cd075d74f
Alin <tdgalin@gmail.com>
2022-06-19 15:02:31 +0300

    authorization made

--- Frontend/admin/MyAccount/index.js
+++ Frontend/admin/MyAccount/index.js
@@ -1,4 +1,37 @@
-$(function(){
-    $("#nav").load("../sidebar/sidebar.html"); 
-  });
+$(function () {
+  $("#nav").load("../sidebar/sidebar.html");
+});
 /*sidebar included*/
+
+
+
+authGet("/user/self", requestOptions);
+
+
+
+async function updateProfile() {
+  try {
+    let response = await fetch(url + "user/profile/" + loggedInUser(), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "type": 1,
+        "email": EmailInput,
+        "password": PasswordInput
+      })
+    })
+    let data = await response.json();
+
+    return data;
+  } catch (err) {
+    return "Error on registering, error type = " + err;
+  }
+}
+
+
+async function update() {
+  await getInputData();
+  console.log(await updateProfile());
+}
