d56aca6df2b235a1addd0763fb576452b2d12314
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-06-20 20:47:26 +0300

    MyAccount integration added

--- Frontend/admin/MyAccount/index.js
+++ Frontend/admin/MyAccount/index.js
@@ -1,34 +1,83 @@
 $(function () {
   $("#nav").load("../sidebar/sidebar.html");
 });
-/*sidebar included*/
+
+(async () => {
+
+  var response = await authGet("/user/self", requestOptions)
+  if (response.success === false) {
+    var update = document.getElementById("update");
+    update.style.display = "none";
+  } else {
+    var create = document.getElementById("create");
+    var username = document.getElementById("username");
+
+    document.getElementsByName("firstName")[0].value = response.data.user.firstName;
+    document.getElementsByName("lastName")[0].value = response.data.user.lastName;
+
+    username.style.display = "none"; // hide create button
+    create.style.display = "none";
+  }
+})();
 
 
-
-async function getInputData() {
+const getInputData = async (create) => {
   window.firstNameInput = document.getElementById("firstName").value;
   window.lastNameInput = document.getElementById("lastName").value;
-  window.usernameInput = document.getElementById("userName").value;
-  window.dateOfBirthInput = document.getElementById("dateOfBirth").value;
+  if (create === true) {
+    window.usernameInput = document.getElementById("username").value;
+  }
+  window.dateOfBirthInput = Date.parse(document.getElementById("dateOfBirth").value);
+
+  if (create === true) {
+    window.body = JSON.stringify({
+      "firstName": window.firstNameInput,
+      "lastName": window.lastNameInput,
+      "username": window.usernameInput,
+      "dateOfBirth": window.dateOfBirthInput,
+    })
+  }
+  else {
+    window.body = JSON.stringify({
+      "firstName": window.firstNameInput,
+      "lastName": window.lastNameInput,
+      "dateOfBirth": window.dateOfBirthInput,
+    })
+  }
+
 }
 
-
-var body = JSON.stringify({
-  "type": 1,
-  "firstName": firstNameInput,
-  "lastName": lastNameInput,
-  "username": usernameInput,
-  "dateOfBirth": dateOfBirthInput,
-  //"email": emailInput  //TODO?
-})
-
-
-async function update() {    //update dar de fapt e post.. 
-  await getInputData();
-  authPost("user/profile/", body);
+const update = async () => {
+  await getInputData(false);
+  var mess = await authPut("/user/self", window.body);
+  alert(mess.message);
 }
 
+const create =  async() => {
+  await getInputData(true);
+  var mess = await authPost("/user/profile", window.body);
+  alert(mess.message);
+}
 
-// authGet("/user/self", requestOptions); //whoAmI  //TObeAddedToURL
+const resetPassword = async () => {
+  try {
+    let response = await fetch(url + "/auth/change_password", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "email": "trydinnou@gmail.com"  // for testing 
+      })
+    })
+    let data = await response.json();
+    if (response.status != 200) {
+      alert(data.message);
+    } else {
+      alert(data.message);
+    }
+  } catch (err) {
+    return "Error on resetting password, error type = " + err;
+  }
 
-//TODO delete account ?
+}
