f4f155e1ecdd5a392c91bee3046125acffec88e5
Bogdan Zavadovschi <bogdanzavadovschi17@gmail.com>
2022-06-20 21:55:55 +0300

    a small fix

--- Backend/services/evaluator/modules/handler/evaluateRules.js
+++ Backend/services/evaluator/modules/handler/evaluateRules.js
@@ -1,10 +1,11 @@
 
 const evaluateRule = async (rule, user) => {
     console.log(rule, user);
+    const type = (rule.match === 'all' ? '&&' : '||');
 }
 exports.execute = async (message) => {
     for(const rule of message.rules) {
-        await evaluateRule(rule, message.user);
+        await evaluateRule(rule, message.listener);
     }
     return message;
 }
